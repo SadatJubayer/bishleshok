5685c29cd5eddf0752d8e573f619ba11d4d74133
Sadat Jubayer <sadatjubayer@gmail.com>
2019-12-15 10:55:34 +0600

    Updates

---
+++ precache-manifest.6e0bed726e3e5a8ce06e9c1c4fefe867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc350ff607257d2bed380a442720821",
+    "url": "/bishleshok/index.html"
+  },
+  {
+    "revision": "1760ea523da23874c1e5",
+    "url": "/bishleshok/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "88d17da8074960448f16",
+    "url": "/bishleshok/static/js/2.7cf907a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bishleshok/static/js/2.7cf907a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1760ea523da23874c1e5",
+    "url": "/bishleshok/static/js/main.c5bc00b4.chunk.js"
+  },
+  {
+    "revision": "178b0dedc37c684b46d1",
+    "url": "/bishleshok/static/js/runtime-main.51aaa2a1.js"
+  }
+]);
